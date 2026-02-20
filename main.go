02abce3c1425f0bc9c4a62a140e80fc1d6e9afc7
JaygnatBuilds <JustinGnatiuk@gmail.com>
2026-02-20 01:46:39 -0500

    initialize REPL and prompt, grab commands

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// initialize REPL loop
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		// Prompt text
+		fmt.Print("Pokedex > ")
+
+		// Scan for commands and capture first string in input
+		scanner.Scan()
+		text := CleanInput(scanner.Text())
+		command := text[0]
+		fmt.Printf("Your command was: %s\n", command)
+
+	}
 }
