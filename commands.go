c79737f35a76db1fa9877a8daff1e2edde72cb69
JaygnatBuilds <justingnatiuk@gmail.com>
2026-07-17 17:35:07 -0400

    pokedex command implementation

--- commands.go
+++ commands.go
@@ -1,6 +1,18 @@
+// REPL command function definitions
+// exit : exit the REPL
+// help : print available commands and usage
+// map : display next 20 map locations
+// mapb : display previous 20 map locations
+// clear : clear console screen
+// explore : list all pokemon in an area
+// catch : attempt to catch a pokemon
+// inspect : inspect a pokemons attributes
+// pokedex : list all pokemon in pokedex
+
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"os"
@@ -14,12 +26,26 @@ type cliCommand struct {
 }
 
 type Pokemon struct {
-	name     string
-	base_exp int
+	Name   string
+	Height int
+	Weight int
+	Stats  []PokemonStat
+	Types  []PokemonType
 }
 
+type PokemonStat struct {
+	Name      string
+	Base_stat int
+}
+
+type PokemonType struct {
+	Name string
+}
+
+// map for storing pokemon
 var pokedex = map[string]Pokemon{}
 
+// exit command : exit the REPL
 func commandExit(cfg *config, param string) error {
 
 	// Parameter check
@@ -33,6 +59,7 @@ func commandExit(cfg *config, param string) error {
 	return nil
 }
 
+// help command : list available commands and usage
 func commandHelp(cfg *config, param string) error {
 
 	// Parameter check
@@ -51,6 +78,7 @@ func commandHelp(cfg *config, param string) error {
 	return nil
 }
 
+// map command : display next 20 map locations from API
 func commandMap(cfg *config, param string) error {
 
 	// Parameter check
@@ -75,6 +103,7 @@ func commandMap(cfg *config, param string) error {
 
 }
 
+// mapb command : display previous 20 map locations from API
 func commandMapb(cfg *config, param string) error {
 
 	// Parameter check
@@ -103,6 +132,7 @@ func commandMapb(cfg *config, param string) error {
 	return nil
 }
 
+// clear command : clear REPL console screen
 func commandClear(cfg *config, param string) error {
 
 	// Parameter check
@@ -118,6 +148,7 @@ func commandClear(cfg *config, param string) error {
 	return nil
 }
 
+// explore command : list all pokemon in an area
 func commandExplore(cfg *config, area string) error {
 
 	data, err := cfg.client.ListPokemonEncounters(area)
@@ -135,8 +166,10 @@ func commandExplore(cfg *config, area string) error {
 	return nil
 }
 
+// catch command : attempt to catch a pokemon
 func commandCatchPokemon(cfg *config, name string) error {
 
+	// call catch pokemon API function
 	data, err := cfg.client.CatchPokemon(name)
 
 	if err != nil {
@@ -148,6 +181,7 @@ func commandCatchPokemon(cfg *config, name string) error {
 		fmt.Printf("%v is already in your pokedex.\n", data.Name)
 		return nil
 	} else {
+		// Simulate catching the pokemon with chances tied to pokemon base exp
 		fmt.Printf("Throwing a Pokeball at %v...\n", data.Name)
 		fmt.Printf("%v base experience is %v\n", data.Name, data.Base_Exp)
 
@@ -159,12 +193,65 @@ func commandCatchPokemon(cfg *config, name string) error {
 
 		catch_roll := rand.Intn(100)
 		fmt.Printf("catch roll : %d\n", catch_roll)
+
+		// If catch is successful, add pokemon to pokedex
 		if catch_roll <= odds_final {
-			fmt.Println("Caught!")
-			pokedex[data.Name] = Pokemon{data.Name, data.Base_Exp}
+
+			fmt.Println("Caught! Adding to PokeDex...")
+			pokemon := Pokemon{}
+			pokemon.Name = data.Name
+			pokemon.Height = data.Height
+			pokemon.Weight = data.Weight
+			for _, s := range data.Stats {
+				stat := PokemonStat{s.Stat.Name, s.BaseStat}
+				pokemon.Stats = append(pokemon.Stats, stat)
+			}
+			for _, s := range data.Types {
+				pokeType := PokemonType{s.Type.Name}
+				pokemon.Types = append(pokemon.Types, pokeType)
+			}
+
+			pokedex[data.Name] = pokemon
+
 		} else {
 			fmt.Println("Miss!")
 		}
+	}
+
+	return nil
+}
+
+// inspect command : inspect a pokemons attributes stored in pokedex
+func commandInspectPokemon(cfg *config, name string) error {
+
+	// parameter check
+	if name == "" {
+		fmt.Println("Name parameter empty, please provide pokemon name with this command.")
+		return nil
+	}
+
+	// check if pokemon is in pokedex
+	if _, ok := pokedex[name]; ok {
+		// convert pokemon object to json bytes
+		data, err := json.MarshalIndent(pokedex[name], "", "	")
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(data))
+	} else {
+		fmt.Println("you have not caught that pokemon...")
+		return nil
+	}
+	return nil
+}
+
+// pokedex command : list all pokemon in pokedex
+func commandInspectPokedex(cfg *config, param string) error {
+
+	// parameter check
+	if param != "" {
+		fmt.Println("Inspect Pokedex command accepts no parameter")
+		return nil
 	}
 
 	return nil
@@ -206,6 +293,16 @@ func getCommands() map[string]cliCommand {
 			name:        "catch",
 			description: "attempt to catch a pokemon",
 			callback:    commandCatchPokemon,
+		},
+		"inspect": {
+			name:        "inspect",
+			description: "inspect a pokemons attributes",
+			callback:    commandInspectPokemon,
+		},
+		"pokedex": {
+			name:        "pokedex",
+			description: "list pokemon in pokedex",
+			callback:    commandInspectPokedex,
 		},
 	}
 }
--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -37,8 +37,22 @@ type PokemonEncounterResponse struct {
 }
 
 type PokemonInfoResponse struct {
+	ID       int    `json:"id"`
 	Name     string `json:"name"`
 	Base_Exp int    `json:"base_experience"`
+	Height   int    `json:"height"`
+	Weight   int    `json:"weight"`
+	Stats    []struct {
+		Stat struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+		BaseStat int `json:"base_stat"`
+	} `json:"stats"`
+	Types []struct {
+		Type struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"types"`
 }
 
 func NewClient(timeout, cacheInterval time.Duration) *Client {
